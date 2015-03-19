232669b9a299e3abd4f735a917e6d99fe3d9372a
Jozef Wagner <jozef.wagner@gmail.com>
2015-03-19 11:46:52 +0100

    preparation for 0.3.7 release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject dunaj-starter "0.1.0-SNAPSHOT"
+(defproject dunaj-starter "0.3.7"
   :description "A starter leiningen project for Dunaj"
   :url "https://github.com/dunaj-project/dunaj-starter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.dunaj/dunaj "1.0.0-SNAPSHOT"]])
+  :dependencies [[org.dunaj/dunaj "0.3.7"]])
