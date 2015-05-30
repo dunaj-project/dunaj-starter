980b033b7c190f78cdd31d52d656fa36a99a7959
Jozef Wagner <jozef.wagner@gmail.com>
2015-05-30 20:54:03 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject dunaj-starter "0.4.0"
+(defproject dunaj-starter "0.5.0"
   :description "A starter leiningen project for Dunaj"
   :url "https://github.com/dunaj-project/dunaj-starter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.dunaj/dunaj "0.4.0"]])
+  :dependencies [[org.dunaj/dunaj "0.5.0"]])
