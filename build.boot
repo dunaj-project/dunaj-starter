aae2656f23606c2c96fad5fe500b844820d499cc
Jozef Wagner <jozef.wagner@gmail.com>
2015-07-28 13:03:22 +0200

    support for boot

---
+++ build.boot
@@ -0,0 +1,4 @@
+(set-env!
+  :resource-paths #{"src"}
+  :dependencies '[[org.dunaj/dunaj "0.7.0"]]
+  :exclusions '[org.clojure/clojure])
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject dunaj-starter "0.6.0"
+(defproject dunaj-starter "0.7.0"
   :description "A starter leiningen project for Dunaj"
   :url "https://github.com/dunaj-project/dunaj-starter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.dunaj/dunaj "0.6.0"]])
+  :dependencies [[org.dunaj/dunaj "0.7.0"]])
